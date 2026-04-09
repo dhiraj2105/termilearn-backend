af94323700dca0e4135a9c8d872a520a8a0fe711
Dhiraj <dkskp2005@gmail.com>
2026-04-09 18:47:18 +0530

    basic api rate limitor

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,24 @@
+import rateLimit from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message:
+      "Too many authentication requests from this IP, please try again after a minute.",
+  },
+});
+
+export const generalLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests from this IP, please try again after a minute.",
+  },
+});
